63e1804ff438124efa10585250dafea4706f12f3
reinoutcl <93149166+reinoutcl@users.noreply.github.com>
2025-10-22 09:14:34 +0200

    ADD: firebase auth

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -3,8 +3,12 @@ import 'API.dart';
 import 'GameLogic.dart';
 import 'components/country.dart';
 import 'pages/comapre.dart';
+
+// Firebase imports
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
 
